ffed11ec041f2db79553c38f76af3f0b52e7cdc1
VedantKalandre <156919810+vedant010@users.noreply.github.com>
2024-02-03 23:16:10 +0530

    Update ProgrammingAssignment1.java

--- ProgrammingAssignment1.java
+++ ProgrammingAssignment1.java
@@ -1,1 +1,35 @@
-dummy
+public class SY2022bit060 {
+    public static void main(String[] args) {
+        SY2022bit060 RegNo = new SY2022bit060();
+        System.out.println(RegNo.getValidRegistrationsCount(args));
+    }
+
+    public static int getValidRegistrationsCount(String[] arguments) {
+        int count = 0;
+        String[] valid_Courses = {"bit", "bcs", "bme", "bch"};
+
+        for (String regNumber : arguments){
+            if (regNumber.length() != 10) 
+                continue;
+
+            String year = regNumber.substring(0, 4);
+            String course = regNumber.substring(4, 7).toLowerCase();
+            String num = regNumber.substring(7, 10);
+
+            int year = Integer.parseInt(year);
+            if (year < 2000 || year > 2024){ 
+                continue;
+            }
+            if (!Arrays.asList(valid_Courses).contains(course)){
+                continue;
+            }
+            if (!num.matches("\\d{3}")){ 
+                continue;
+            }
+            count++;
+        }
+
+        return count;
+    }
+}
+
